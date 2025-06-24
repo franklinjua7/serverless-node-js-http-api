de507fccc9fa3c01fb7f85506d1585f833319440
Franklin Juárez <franjua.123@gmail.com>
2025-06-23 19:36:33 -0500

    Adición de la funcionalidad de almacenar.

--- src/almacenar.js
+++ src/almacenar.js
@@ -1,10 +1,28 @@
 const AWS = require('aws-sdk');
 
-const almacenar = (event) => {
+const almacenar = async (event) => {
+
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const { characterName, planeta, climate } = JSON.parse(event.body);
+    const timestamp = Math.floor(new Date().getTime()/1000.0);
+
+    const data_almacenar = {
+        characterName,
+        timestamp,
+        climate,
+        planeta
+    };
+
+    console.log('Data almacenar: ', data_almacenar);
+
+    await dynamodb.put({
+        TableName: 'RetoTecnicoTable',
+        Item: data_almacenar
+    }).promise();
 
     return {
         status: 200,
-        body : {}
+        body : JSON.stringify(data_almacenar)
     }
 }
 
